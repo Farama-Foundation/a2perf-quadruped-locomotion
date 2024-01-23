6601bbfa60c51d85de0888f848709e4a0293613f
Ikechukwu Uchendu <iuchendu@g.harvard.edu>
2024-01-23 17:49:24 +0000

    Modified how motion files are found by default

--- __init__.py
+++ __init__.py
@@ -1,29 +1,39 @@
 import gin
 import gym as legacy_gym
 import gymnasium as gym
-import os
+import pkg_resources
 
-config_file_path = os.path.join(os.path.dirname(__file__), 'motion_imitation',
-                                'configs', 'envdesign.gin')
+# Construct the path to the config file using pkg_resources
+config_file_path = pkg_resources.resource_filename(
+    'a2perf',
+    'domains/quadruped_locomotion/motion_imitation/configs/envdesign.gin'
+)
 
 gin.parse_config_files_and_bindings([config_file_path], None,
                                     finalize_config=False)
 
+# Construct the path to the motion file using pkg_resources
+motion_file_path = pkg_resources.resource_filename(
+    'a2perf',
+    'domains/quadruped_locomotion/motion_imitation/data/motions/dog_pace.txt'
+)
+
+# Register the environments with the relative path
 gym.envs.register(
     id='QuadrupedLocomotion-v0',
     apply_api_compatibility=True,
     entry_point='a2perf.domains.quadruped_locomotion.motion_imitation.envs.env_builder:build_imitation_env',
     kwargs={
-        'motion_files': [
-            "/rl-perf/a2perf/domains/quadruped_locomotion/motion_imitation/data/motions/dog_pace.txt"],
-        'enable_rendering': False, 'mode': 'train'}
+        'motion_files': [motion_file_path],
+        'enable_rendering': False, 'mode': 'train'
+    }
 )
 
 legacy_gym.envs.register(
     id='QuadrupedLocomotion-v0',
     entry_point='a2perf.domains.quadruped_locomotion.motion_imitation.envs.env_builder:build_imitation_env',
     kwargs={
-        'motion_files': [
-            "/rl-perf/a2perf/domains/quadruped_locomotion/motion_imitation/data/motions/dog_pace.txt"],
-        'enable_rendering': False, 'mode': 'train'}
+        'motion_files': [motion_file_path],
+        'enable_rendering': False, 'mode': 'train'
+    }
 )
