a8c6643f94d655a6548cadd25ff45c04e08de9b2
Ikechukwu Uchendu <iuchendu@g.harvard.edu>
2024-07-20 15:08:16 -0400

    created separate envs for each gait. removed legacy gym requirement

--- __init__.py
+++ __init__.py
@@ -1,5 +1,4 @@
 import gin
-import gym as legacy_gym
 import gymnasium as gym
 import pkg_resources
 
@@ -7,29 +6,38 @@ config_file_path = pkg_resources.resource_filename(
     "a2perf", "domains/quadruped_locomotion/motion_imitation/configs/envdesign.gin"
 )
 gin.parse_config_files_and_bindings([config_file_path], None, finalize_config=False)
-motion_file_path = pkg_resources.resource_filename(
-    "a2perf", "domains/quadruped_locomotion/motion_imitation/data/motions/dog_pace.txt"
-)
 
-gym.envs.register(
-    id="QuadrupedLocomotion-v0",
-    apply_api_compatibility=True,
-    entry_point="a2perf.domains.quadruped_locomotion.motion_imitation.envs.env_builder:build_imitation_env",
-    kwargs={
-        "motion_files": [motion_file_path],
-        "enable_rendering": False,
-        "mode": "train",
-        "num_parallel_envs": 1,
-    },
-)
+motion_files = {
+    "pace": pkg_resources.resource_filename(
+        "a2perf",
+        "domains/quadruped_locomotion/motion_imitation/data/motions/dog_pace.txt",
+    ),
+    "trot": pkg_resources.resource_filename(
+        "a2perf",
+        "domains/quadruped_locomotion/motion_imitation/data/motions/dog_trot.txt",
+    ),
+    "spin": pkg_resources.resource_filename(
+        "a2perf",
+        "domains/quadruped_locomotion/motion_imitation/data/motions/dog_spin.txt",
+    ),
+}
 
-legacy_gym.envs.register(
-    id="QuadrupedLocomotion-v0",
-    entry_point="a2perf.domains.quadruped_locomotion.motion_imitation.envs.env_builder:build_imitation_env",
-    kwargs={
-        "motion_files": [motion_file_path],
-        "enable_rendering": False,
-        "mode": "train",
-        "num_parallel_envs": 1,
-    },
-)
+
+def register_env(gait, motion_file):
+    env_id = f"QuadrupedLocomotion-Dog{gait.capitalize()}-v0"
+
+    gym.envs.register(
+        id=env_id,
+        apply_api_compatibility=True,
+        entry_point="a2perf.domains.quadruped_locomotion.motion_imitation.envs.env_builder:build_imitation_env",
+        kwargs={
+            "motion_files": [motion_file],
+            "enable_rendering": False,
+            "mode": "train",
+            "num_parallel_envs": 1,
+        },
+    )
+
+
+for gait, motion_file in motion_files.items():
+    register_env(gait, motion_file)
